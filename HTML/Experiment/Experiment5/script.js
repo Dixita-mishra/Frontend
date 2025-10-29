2d8c118c98c53db56a46e3c9fbf7d9bf558b31cb
Dixita-mishra <dixitamishra23@gmail.com>
2025-10-29 09:46:03 +0530

    Experiment5 completed

---
+++ HTML/Experiment/Experiment5/script.js
@@ -0,0 +1,12 @@
+//const username=prompt("Welcome to this website,what's your name?");
+//alert("Hello "+username+" ,have a nice day!");
+
+
+const age=prompt("Enter your age");
+//age>=18?alert("you are an eligible voter"):alert("you are not an eligible voter");
+//console.log(typeof age);
+//console.log("a"*2);
+//console.log("2"+ age );
+age>=18
+?document.write("<h1>you are an adult</h1>")
+:document.write("<h1>you are a minor</h1>");
