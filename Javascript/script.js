c769dfdc17347c7ca7079ea9c40d6981362d601e
Dixita-mishra <dixitamishra23@gmail.com>
2025-10-30 11:53:02 +0530

    array methods discussed

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,4 +1,4 @@
-//function check(age){
+ //function check(age){
   //  if(age>13 && age<=19){
   //      console.log("Teenager");
     ///}
@@ -14,37 +14,51 @@
 //     }
 //     console.log(str);
 // }
-function calculate(operation,num1,num2,...rest){
-  let result;
-  switch(operation){
-    case 'add': {
-      result=0;for(i of rest){
-      result+=i;
-    }return result+=num1+num2;}
-    case 'multiply': {
-      result=1;
-      for(i of rest){
-        result*=i;
-      }return result*=num1*num2;
-    }
-    case 'subtract' :{
-      result=0;
-      for(i of rest){
-   result-=i;
-      }
-      return result-=num1-num2;
-    }
-    case 'divide':{
-      result=1;
-      for(i of result)
-        {
-          result/=i;
-    }
-    return result/=num1/num2;
-  }
-    default: return "operation not recognized";
+// function calculate(operation,num1,num2,...rest){
+//   let result;
+//   switch(operation){
+//     case 'add': {
+//       result=0;for(i of rest){
+//       result+=i;
+//     }return result+=num1+num2;}
+//     case 'multiply': {
+//       result=1;
+//       for(i of rest){
+//         result*=i;
+//       }return result*=num1*num2;
+//     }
+//     case 'subtract' :{
+//       result=0;
+//       for(i of rest){
+//    result-=i;
+//       }
+//       return result-=num1-num2;
+//     }
+//     case 'divide':{
+//       result=1;
+//       for(i of result)
+//         {
+//           result/=i;
+//     }
+//     return result/=num1/num2;
+//   }
+//     default: return "operation not recognized";
 
-    }
+//     }
 
-  }
-  console.log(calculate('add',1,3,3,4,5,));
+//   }
+//   console.log(calculate('add',1,3,3,4,5,));
+
+//Array methods
+const arr=[1,2,3,4];
+// arr.shift(); //removes first element
+// console.log(arr);
+// arr.unshift(0);//adds element at the begininng
+// console.log(arr);
+const arr2=[5,6,7,8,3];
+const newArr=arr.concat(arr2);//combines two arrays
+console.log(newArr);
+const index5=newArr.indexOf(5);//gives index of element
+console.log(index5);
+const lastIndex=newArr.lastIndexOf(3);//gives last index of element
+console.log(lastIndex);
